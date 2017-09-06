26aeb303ddddf64ed9a172f1669f2ffeb10056e7
= <=>
2017-09-06 14:57:25 +0530

    update q1 and q2

--- assignment1/q1.sql
+++ assignment1/q1.sql
@@ -12,3 +12,10 @@ from film
                 where category.name = 'Comedy'
 			)
 		);
+        
+select f.film_id,f.title 
+	from category c , film_category fc, film f
+		where c.category_id = fc.category_id
+        and c.name = 'Comedy'
+        and fc.film_id = f.film_id
+        and f.rating ='PG-13';
--- assignment1/q2.sql
+++ assignment1/q2.sql
@@ -39,3 +39,12 @@ select f.title, f.film_id, count(all i.film_id)
 		where 
 			c.category_id = fc.category_id and c.name = 'Horror'  and fc.film_id= i.film_id and i.film_id = f.film_id
             Group by i.film_id order by count(all i.film_id) desc, title asc limit 3;
+            
+
+--  correct answer
+select fl.title , count(all fl.title)
+	from film_list fl, inventory i, rental r
+		where fl.category = 'Horror'
+        and fl.FID = i.film_id
+        and i.inventory_id = r.inventory_id
+        Group by title order by count(all fl.title) desc limit 3;
